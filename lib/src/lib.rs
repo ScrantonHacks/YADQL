e6ec6fc6bc126bd6f180b3cd317b87be00e2abf3
Andrew Plaza <aplaza@liquidthink.net>
2018-01-06 11:59:47 -0500

    General Parser finished, no more main error

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,3 +1,16 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate colored;
+
+mod core;
+mod parser;
+
+use std::process;
+use std::error::Error;
+use colored::Colorize;
+
+use parser::parser::Parser;
+
 #[cfg(test)]
 mod tests {
     #[test]
---
+++ lib/src/parser/err.rs
@@ -0,0 +1,88 @@
+use std::io;
+use std::error;
+use std::fmt;
+
+use parser::parser::yadql_grammar::ParseError;
+
+
+#[derive(Debug)]
+pub struct KeywordNotFound {
+    pub v: string,
+}
+
+pub type KeywordNotFoundError = KeywordNotFound;
+
+impl fmt::Display for KeywordNotFoundError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.v)
+    }
+}
+
+impl error::Error for KeywordNotFoundError {
+    fn description(&self) -> &str {
+        "The Keyword Could Not Be Identified"
+    }
+}
+
+#[derive(Debug)]
+pub enum ParserError {
+    CouldNotOpenSourceCode(io::Error),
+    CouldNotParseFile(ParseError),
+    KeywordNotFound(KeywordNotFoundError),
+}
+
+impl fmt::Display for ParserError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    
+        match *self {
+            ParserError::CouldNotOpenSourceCode(ref err) => {
+                write!(f, "Could not open Source Code File! {}", err)
+            }
+            ParserError::CouldNotParseFile(ref err) => {
+                write!(f, "Was not able to parse YADQL {}", err)
+            }
+            ParserError::KeywordNotFound(ref err) => {
+                write!(f, "Could Not Find Keyword {}", err)
+            }
+        
+        }
+    }
+}
+
+impl error::Error for ParserError {
+    fn description(&self) -> &str {
+        match *self {
+            ParserError::CouldNotOpenSourceCode(ref err) => err.description(),
+            ParserError::CouldNotParseFile(ref err) => err.description(),
+            ParserError::KeywordNotFound(ref err) => err.description(),
+        }
+    }
+   
+    fn cause(&self) -> Option<&error::Error> {
+        match *self {
+            ParserError::CouldNotOpenSourceCode(ref err) => Some(err),
+            ParserError::CouldNotParseFile(ref err) => Some(err),
+            ParserError::KeywordNotFound(ref err) => Some(err),
+        }
+    }
+}
+
+impl From<io::Error> for ParserError {
+    fn from(err: io::Error) -> ParserError {
+        ParserError::CouldNotOpenSourceCode(err)
+    }
+}
+
+impl From<ParseError> for ParserError {
+    fn from(err: ParseError) -> ParserError {
+        ParserError::CouldNotParseFile(err)
+    }
+}
+
+impl From<KeywordNotFoundError> for ParserError {
+    fn from(err: KeywordNotFoundError) -> ParserError {
+        ParserError::KeywordNotFoundError(err)
+    }
+}
+
+
