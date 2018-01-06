cb7f39e4785b44790a7cbf274badb1628c08e948
Sean Batzel <romulus108@protonmail.com>
2018-01-06 18:56:58 -0500

    wow

--- lib/target/debug/build/YADQL-35f63e5a22c28325/build_script_build-35f63e5a22c28325.d
+++
@@ -1,5 +0,0 @@
-/home/sean/Documents/YADQL/lib/target/debug/build/YADQL-35f63e5a22c28325/build_script_build-35f63e5a22c28325: src/build.rs
-
-/home/sean/Documents/YADQL/lib/target/debug/build/YADQL-35f63e5a22c28325/build_script_build-35f63e5a22c28325.d: src/build.rs
-
-src/build.rs:
