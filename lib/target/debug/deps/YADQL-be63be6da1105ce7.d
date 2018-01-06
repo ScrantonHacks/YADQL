781d2bb44c49405ca86b70a1b2d39b1f4573f422
Andrew Plaza <aplaza@liquidthink.net>
2018-01-06 12:00:34 -0500

    General Parser finished, no more main error

--- lib/target/debug/deps/YADQL-be63be6da1105ce7.d
+++
@@ -1,9 +0,0 @@
-/home/insi/Projects/YADQL/lib/target/debug/deps/libYADQL-be63be6da1105ce7.rlib: src/lib.rs src/core/mod.rs src/core/keywords.rs src/core/err.rs src/parser/mod.rs
-
-/home/insi/Projects/YADQL/lib/target/debug/deps/YADQL-be63be6da1105ce7.d: src/lib.rs src/core/mod.rs src/core/keywords.rs src/core/err.rs src/parser/mod.rs
-
-src/lib.rs:
-src/core/mod.rs:
-src/core/keywords.rs:
-src/core/err.rs:
-src/parser/mod.rs:
