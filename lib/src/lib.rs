406ee2d5981217c78ff0a7b4c26ed543c98c50c0
Sean Batzel <romulus108@protonmail.com>
2018-01-06 18:37:34 -0500

    Started laying out blockchain interactions.

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,7 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+mod yadql {
+
+    struct yadql {}
+
+    impl yadql {
+        fn execute(&self, statement: str) -> str {
+            let cmd = parse(statement);
+            let ret = '';
+            match cmd.OP {
+                YADQL::Insert => Blockchain.insert(cmd.arg0, cmd.arg1),
+                YADQL::Delete => Blockchain.delete(cmd.arg0),
+                YADQL::Update => Blockchain.update(cmd.arg0, cmd.arg1),
+                YADQL::Read => {
+                    ret = Blockchain.read(cmd.arg0);
+                },
+            }
+            ret
+        }
+    }
+
+    fn open() {
+        return yadql {};
     }
 }
