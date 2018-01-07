736e0bb57abd6f2daec497bc712d5800aed786de
Andrew Plaza <aplaza@liquidthink.net>
2018-01-07 06:03:05 -0500

    integrate hashing and web3

--- contract/contracts/YadqlStorage.sol
+++ contract/contracts/YadqlStorage.sol
@@ -2,9 +2,9 @@ pragma solidity ^0.4.0;
 import "./Storage.sol";
 
 contract YadqlStorage {
-  mapping (bytes4 => Storage) public db;
+  mapping (bytes16 => Storage) public db;
   
-  function store(bytes4 pubkey, bytes32 key, string val) public {
+  function store(bytes16 pubkey, bytes32 key, string val) public {
     if (db[pubkey] != address(0x0)) {
       db[pubkey].store(key, val);
     } else {
--- lib/src/blockchain/blockchain.rs
+++ lib/src/blockchain/blockchain.rs
@@ -31,14 +31,17 @@ pub struct Success {
     pub payload: String
 }
 
+fn init_web3(provider: &str) -> web3::Web3 {
+    let (_eloop, transport) = web3::transports::Http::new(provider).unwrap(); 
+    let web3 = web3::Web3::new(transport); 
+    let accounts = web3.eth().accounts().wait().unwrap();
+    web3
+}
+
 impl Blockchain {
     //! # impl Blockchain
     //! Represents the current state of the blockchain. This honestly shouldn't care if we're submitting or receiving the queries; it should be able to handle upload as well as download. 
     pub fn new(provider: &str) -> Blockchain {
-        let (_eloop, transport) = web3::transports::Http::new(provider).unwrap(); 
-        let web3 = web3::Web3::new(transport); 
-        let accounts = web3.eth().accounts().wait().unwrap();
-
         let mut file = File::open("contract_address").unwrap();
         let mut contents = String::new();
         let addrutf8: String = file.read_to_string(&mut contents).unwrap().to_string();
@@ -48,31 +51,38 @@ impl Blockchain {
         // let storg_contract = web3::contract::Contract::new(web3.eth(), cAddr, ethabiC);
         
         Blockchain {
+            provider,
             cAddr,
             ethabi: ethabiC,
             memory: Vec::new()
         }
     }
 
-    pub fn insert(&mut self, key: &str, value: &str) -> Result<Success, BlockchainError> {
+    pub fn insert(&mut self, pubkey: &str, key: &str, value: &str) -> Result<Success, BlockchainError> {
         //! ## insert(key: &str, value: &str) -> Result<Success, BlockchainError>
         //! Inserts a new value into the blockchain.
         
         //! Should throw an error if the record already exists.
         self.memory.push(KeyVal { key: key.to_string(), val: value.to_string() });
+        let web3 = init_web3(self.provider);
+        let storg = web3::contract::Contract::new(web3.eth(), self.cAddr, self.ethabi);
+        storg.query("store", (calculate_hash(pubkey), key, value), None, Options::default(), None);
         Ok(Success { payload: self.memory[self.memory.len() - 1].val.clone() })
     }
 
-    pub fn delete(&mut self, key: &str) -> Result<Success, BlockchainError>{
+    pub fn delete(&mut self, pubkey: &str, key: &str) -> Result<Success, BlockchainError>{
         //! ## delete(key: &str) -> Result<Success, BlockchainError>
         //! Marks a record as deleted.
         //! Should fail if the record doesn't exist.
         let ind = self.memory.iter().position(|ref r| r.key == key).unwrap();
         self.memory.remove(ind);
+        let web3 = init_web3(self.provider);
+        let storg = web3::contract::Contract::new(web3.eth(), self.cAddr, self.ethabi);
+        storg.query("store", (calculate_hash(pubkey), key, ""), None, Options::default(), None);
         Ok(Success { payload: "".to_string() })
     }
 
-    pub fn update(&mut self, key: &str, value: &str) -> Result<Success, BlockchainError> {
+    pub fn update(&mut self, pubkey: &str, key: &str, value: &str) -> Result<Success, BlockchainError> {
         //! ## update(key: &str, value: &str) -> Result<Success, BlockchainError>
         //! Updates a value in the local database and submits to the blockchain.
         //! Should fail if the record doesn't exist.
@@ -85,10 +95,14 @@ impl Blockchain {
                 break 'update;
             }
         }
+        let web3 = init_web3(self.provider);
+        let storg = web3::contract::Contract::new(web3.eth(), self.cAddr, self.ethabi);
+        storg.query("store", (calculate_hash(pubkey), key, value), None, Options::default(), None);
+
         Ok(ret)
     }
 
-    pub fn read(&self, key: &str) -> Result<Success, BlockchainError> {
+    pub fn read(&self, pubkey: &str, key: &str) -> Result<Success, BlockchainError> {
         //! ## read(key: &str) -> Result<Success, BlockchainError>
         //! A method on the Blockchain struct that reads data from memory into the application's context for use. This really just worries about the local mirror of the database; we really don't care about what the blockchain looks like.
         //! Should fail if the record doesn't exist.
