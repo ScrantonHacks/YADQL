2a33ca2774789e49ab788ee405c2328cb64f0b83
Andrew Plaza <aplaza@liquidthink.net>
2018-01-07 00:29:14 -0500

    seperated send/recv into stream

--- lib/src/blockchain/blockchain.rs
+++ lib/src/blockchain/blockchain.rs
@@ -7,8 +7,8 @@ use parser::parser::Parser;
 
 struct KeyVal {
     /// # KeyVal
-    key: i32,
-    val: i32
+    key: String,
+    val: String,
 }
 
 pub struct Blockchain {
@@ -30,64 +30,11 @@ impl Blockchain {
         }
     }
 
-    pub fn send(&self, operation: &str, key: &str, value: &str) {
-        //! ## send(operation: &str, key: &str, value: &str)
-        //! Applies transactions being sent from this machine.
-        //! It was late when I wrote this... needs fixing bad.
-        let res = match operation {
-            YADQL::Insert(ref k, ref v) => {
-                insert(key, value);
-                let c = Crypt::new(String::from("test@radical-yadql.io")); // THIS CANNOT BE THE FINAL EMAIL.
-                let payload = format!("('operation': 'insert', key: '{}', value: '{}' )", key, value);
-                let crypt_sign = c.sign(c.encrypt(String::from(payload)));
-                // TODO Send crypt_sign to the blockchain.
-            },
-            YADQL::Delete(ref k) => {
-                delete(key);
-                let c = Crypt::new(String::from("test@radical-yadql.io")); // THIS CANNOT BE THE FINAL EMAIL.
-                let payload = format!("('operation': 'delete', key: '{}', value: '{}' )", key, value);
-                let crypt_sign = c.sign(c.encrypt(String::from(payload)));
-                // TODO Send crypt_sign to the blockchain.
-            },
-            YADQL::Update(ref k, ref v) => {
-                update(key, value);
-                let c = Crypt::new(String::from("test@radical-yadql.io")); // THIS CANNOT BE THE FINAL EMAIL.
-                let payload = format!("('operation': 'update', key: '{}', value: '{}' )", key, value);
-                let crypt_sign = c.sign(c.encrypt(String::from(payload)));
-                // TODO Send crypt_sign to the blockchain.
-            },
-            YADQL::Read(ref k) => {
-                read(key)
-            },
-        };
-    }
-
-    pub fn recv(&self) {
-        //! ## recv()
-        //! Applies transactions downloaded to this machine.
-        let next_query = String::new(); // This is a placeholder. Make sure we get this one from the EVM.
-        let c = Crypt::new(String::from("test@radical-yadql.io")); // THIS CANNOT BE THE FINAL EMAIL.
-        let payload = c.decrypt(c.verify(next_query));
-        // We need to be able to parse ( 'operation': '', ‘key’: ‘’, ‘value’: ‘’ )
-        let parser: Parser = parse(payload); // We need a second parser for this, this will be a stand-in for now.
-        let ret = match *parser.keywords.get(0).unwrap() (
-            YADQL::Insert(ref k, ref v) => {
-                insert(k, v;
-            },
-            YADQL::Delete(ref k) => {
-                delete(k);
-            },
-            YADQL::Update(ref k, ref v) => {
-                update(k, v;
-            },
-        )
-    }
-
     pub fn insert(&self, key: &str, value: &str) -> Result<Success, BlockchainError> {
         //! ## insert(key: &str, value: &str) -> Result<Success, BlockchainError>
         //! Inserts a new value into the blockchain.
         //! Should throw an error if the record already exists.
-        self.memory.push(KeyVal {key, val});
+        self.memory.push(KeyVal {key.to_string(), val.to_string()});
     }
 
     pub fn delete(&self, key: &str) -> Result<Success, BlockchainError> {
