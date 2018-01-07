8d979cb58c13bb09872903ab4803d5cbbb870c57
Sean Batzel <romulus108@protonmail.com>
2018-01-06 20:59:46 -0500

    Still working on GPG stuff.

--- lib/src/blockchain.rs
+++ lib/src/blockchain.rs
@@ -1,20 +1,34 @@
 mod Blockchain {
-    fn insert(key, val) {
+    //! # mod Blockchain
+    //! Allows interaction with the Ethereum blockchain.
+    fn insert(key: str, val: str) -> str {
+        //! ## insert(key: str, val: str) -> str
+        //! Takes a key and value and inserts them into the blockchain.
+        //! Returns completed status as a string.
         // encrypt
         // send
     }
     
-    fn update(key, val) {
+    fn update(key: str, val: str) -> str {
+        //! ## update(key: str, val: str) -> str
+        //! Updates an existing value in the blockchain.
+        //! Returns completed status as a string.
         // encrypt
         // send
     }
     
-    fn delete(key) {
+    fn delete(key: str) -> str {
+        //! ## delete(key: str) -> str
+        //! Removes an existing value from the blockchain.
+        //! Returns completed status as a string.
         // encrypt
         // send
     }
     
-    fn read(key) {
+    fn read(key: str) -> str {
+        //! ## read(key: str) -> str
+        //! Reads the value associated with `key` from the blockchain.
+        //! Returns either the value if it exists, or an empty string if it doesn't.
         // can I verify this signature?
         // yes?
         //  can I decrypt it?
